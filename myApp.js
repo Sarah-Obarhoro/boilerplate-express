8ff34b0a1910f7e03a4366bb39f4571bef6b3f26
Sarah-Obarhoro <105218020+Sarah-Obarhoro@users.noreply.github.com>
2024-03-11 07:18:33 +0100

    Served a static asset

--- myApp.js
+++ myApp.js
@@ -5,9 +5,11 @@ let app = express();
 //   res.send("Hello Express");
 // });
 
-app.get("/", function (req, res) {
-  res.sendFile((absolutePath = __dirname + "/views/index.html"));
-});
+// app.get("/", function (req, res) {
+//   res.sendFile((absolutePath = __dirname + "/views/index.html"));
+// });
+
+app.use("/public", express.static( __dirname + "/public"));
 
 
 
