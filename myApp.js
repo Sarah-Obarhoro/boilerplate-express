1daf8425f9294191ff98727632cedc8bcbdf8b54
Sarah-Obarhoro <105218020+Sarah-Obarhoro@users.noreply.github.com>
2024-03-11 07:38:40 +0100

    Made edits to effectively render static file

--- myApp.js
+++ myApp.js
@@ -5,9 +5,9 @@ let app = express();
 //   res.send("Hello Express");
 // });
 
-// app.get("/", function (req, res) {
-//   res.sendFile((absolutePath = __dirname + "/views/index.html"));
-// });
+app.get("/", function (req, res) {
+  res.sendFile((absolutePath = __dirname + "/views/index.html"));
+});
 
 app.use("/public", express.static( __dirname + "/public"));
 
