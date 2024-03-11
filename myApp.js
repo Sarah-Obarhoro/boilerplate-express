482db50aa425d38173946e3a5a7ea095fab1eef3
Sarah-Obarhoro <obarhorosarah@gmail.com>
2024-03-11 08:13:10 +0100

    learnt to create and use .env

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require("dotenv").config();
 
 // app.get("/", function (req, res) {
 //   res.send("Hello Express");
@@ -13,6 +14,11 @@ app.use("/public", express.static( __dirname + "/public"));
 
 app.get("/json", function (req, res) {
   res.json({ message: "Hello json" });
+  if (process.env.VAR_NAME === "allCaps") {
+    response = "Hello World".toUpperCase();
+  } else {
+    response = "Hello World";
+  }
 });
 
 
