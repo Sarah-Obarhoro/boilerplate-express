346f4ac1cf9ab54a8fc68626eec01b73ad7bbb54
Sarah-Obarhoro <105218020+Sarah-Obarhoro@users.noreply.github.com>
2024-03-11 06:56:18 +0100

    Served an HTML file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function (req, res) {
-    res.send("Hello Express");   
-})
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
+
+app.get("/", function (req, res) {
+  res.sendFile((absolutePath = __dirname + "/views/index.html"));
+});
 
 
 
